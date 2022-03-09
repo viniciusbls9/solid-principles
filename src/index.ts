97486d99e80e304bc82cb05fbb2fd9e66e022fe9
Vinicius Benedito <beneditoloura@gmail.com>
2022-03-09 07:14:40 -0300

    feat: calculate the area of Square and Circle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import AreaCalculator from './01_S/area-calculator';
+import Square from './01_S/square';
+import Circle from './01_S/circle';
+
 (() => {
-  console.log('hello')
-})()
+  const square = new Square(2);
+  const circle = new Circle(2);
+  const areaCalculator = new AreaCalculator([square, circle]);
+  console.log('O1_S principle: ', areaCalculator.sum());
+})();
