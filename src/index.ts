f6139b4f2f86e77f6a6aa4e0a4db74fea32f26e5
Vinicius Benedito <beneditoloura@gmail.com>
2022-03-14 09:24:23 -0300

    feat: use AreaCalculator with Open closed principles

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import AreaCalculator from './01_S/AreaCalculator/area-calculator';
 import Square from './01_S/Square/square';
 import Circle from './01_S/Circle/circle';
+import Triangle from './02_O/Triangle/triangle';
 
 (() => {
   const square = new Square(2);
   const circle = new Circle(2);
   const areaCalculator = new AreaCalculator([square, circle]);
   console.log('O1_S principle: ', areaCalculator.sum());
+
+  const triangle = new Triangle(10, 2);
+  const areaCalculatorWithOpenClosed = new AreaCalculator([square, circle, triangle]);
+  console.log('O2_O principle: ', areaCalculatorWithOpenClosed.sum());
 })();
