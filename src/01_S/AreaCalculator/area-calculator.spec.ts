a6371f638e45b5aa4d6c795f74799dd084ed807a
Vinicius Benedito <beneditoloura@gmail.com>
2022-03-14 09:16:28 -0300

    refactor: change files imports

--- src/01_S/AreaCalculator/area-calculator.spec.ts
+++ src/01_S/AreaCalculator/area-calculator.spec.ts
@@ -1,13 +1,12 @@
-import Circle from "../Circle/circle";
-import Square from "../Square/square";
+import Circle from "01_S/Circle/circle";
+import Square from "01_S/Square/square";
 import AreaCalculator from "./area-calculator";
 
-describe('Circle Class', () => {
+describe('AreaCalculator Class', () => {
   test('should calculate the area of the circle', () => {
     const square = new Square(2);
     const circle = new Circle(2);
-
-    const areaCalculator = new AreaCalculator([square, circle]);
+    const areaCalculator = new AreaCalculator([circle, square]);
 
     expect(Math.floor(areaCalculator.sum())).toBe(16)
   })
