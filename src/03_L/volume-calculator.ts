77d4412b6bb75861db95476900bab8e5c16a8a5b
Vinicius <beneditoloura@gmail.com>
2022-03-14 21:19:02 -0300

    feat: create volume calculator using the Liskov Substitution Principle (L)

---
+++ src/03_L/volume-calculator.ts
@@ -0,0 +1,9 @@
+import AreaCalculator from '../01_S/AreaCalculator/area-calculator';
+
+class VolumeCalculator extends AreaCalculator{
+  sum (): string {
+    return 'Here comes our volume logic and returns the sum of it';
+  }
+}
+
+export default VolumeCalculator;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import AreaCalculator from './01_S/AreaCalculator/area-calculator';
 import Square from './01_S/Square/square';
 import Circle from './01_S/Circle/circle';
 import Triangle from './02_O/Triangle/triangle';
+import VolumeCalculator from './03_L/volume-calculator';
 
 (() => {
   const square = new Square(2);
@@ -12,4 +13,7 @@ import Triangle from './02_O/Triangle/triangle';
   const triangle = new Triangle(10, 2);
   const areaCalculatorWithOpenClosed = new AreaCalculator([square, circle, triangle]);
   console.log('O2_O principle: ', areaCalculatorWithOpenClosed.sum());
+
+  const volumeCalculator = new VolumeCalculator([square, circle, triangle]);
+  console.log('03_L principle: ', volumeCalculator.sum());
 })();
