6c282e9cb4497de9fcffb738cf4b70bd1fb16c2e
Vinicius <beneditoloura@gmail.com>
2022-03-14 21:39:38 -0300

    feat: create shapes interface

--- src/models/shapesInterface.ts
+++ src/models/shapesInterface.ts
@@ -1,7 +1,8 @@
-import Square from '01_S/Square/square'
-import Circle from '01_S/Circle/circle';
-import Triangle from '02_O/triangle';
+import Square from '../01_S/Square/square'
+import Circle from '../01_S/Circle/circle';
+import Triangle from '../02_O/triangle/triangle';
+import Cuboid from '../04_I/cuboid';
 
 export interface ShapesInterface {
-  [index:number]: Square|Circle|Triangle;
+  [index:number]: Square|Circle|Triangle|Cuboid;
 }
